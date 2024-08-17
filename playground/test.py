942d090080726351d79d5e8b96727d7824dd9e11
yonghun16 <yonghun16@naver.com>
2024-08-17 20:15:58 +0900

    update

--- playground/test.py
+++ playground/test.py
@@ -1,10 +1,11 @@
-x, y = map(int, input().split())
+x = int(input()) 
+y = int(input()) 
 
 if x > 0 and y > 0 :
     print("1")
 elif x < 0 and y > 0 :
     print("2")
-elif x < 1 and y < 0 :
+elif x < 0 and y < 0 :
     print("3")
 else :
     print("4")
