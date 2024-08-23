908fa2fcb09763994354f9bd9f3206197385dfaa
yonghun16 <yonghun16@naver.com>
2024-08-23 13:23:26 +0900

    update

---
+++ 3_Sorting_and_searching/qsort.c
@@ -0,0 +1,48 @@
+/* ----------------------------------------
+- [BOJ] 1448. 삼각형 만들기(https://www.acmicpc.net/problem/1448)
+- Tag: c, greedy, sort
+-------------------------------------------
+- Memo:
+  - qsort의 다양한 정렬 조건(compare)
+    - 오름차순 정렬: compare 함수에서 int_a < int_b일 때 음수를 반환
+    - 내림차순 정렬: compare 함수에서 int_a > int_b일 때 음수를 반환
+    - qsort(array, n, sizeof(int), compare);
+-----------------------------------------*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    return *(int*)b - *(int*)a; // 내림차순 정렬
+}
+
+int main() {
+  int n, a, b, c;
+  int result = -1;
+
+  scanf("%d", &n);
+
+  int *sideOfTriangle = (int *)malloc(n * sizeof(int));
+
+  for(int i = 0; i < n; i++) {
+    scanf("%d", &sideOfTriangle[i]);
+  }
+  qsort(sideOfTriangle, n, sizeof(int), compare);
+
+  for(int i = 0; i < n-2; i++) {
+    a = sideOfTriangle[i];
+    b = sideOfTriangle[i+1];
+    c = sideOfTriangle[i+2];
+    if(a < b + c) {
+      result = a + b + c;
+      break;
+    }
+  }
+
+  free(sideOfTriangle);
+
+  printf("%d\n", result);
+
+  return 0;
+}
