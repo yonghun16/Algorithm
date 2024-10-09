6d3854e52f667f1acccf2a67d6b04a4de22071ba
yonghun16 <yonghun16@naver.com>
2024-10-09 11:32:19 +0900

    update

---
+++ playground/boj_11718/boj_11718.py
@@ -0,0 +1,14 @@
+'''
+----------------------------------------------
+Sub  : [BOJ] 그대로 출력하기
+Link : https://www.acmicpc.net/problem/11718
+Tag  : python, 문자열
+Memo
+----------------------------------------------
+'''
+
+import sys
+
+# 입력을 한꺼번에 읽어서 출력
+input_data = sys.stdin.read()
+print(input_data)
