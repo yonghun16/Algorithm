dace11954b4fbe7d788c12dbf010fbac13cfb0b1
yonghun16 <yonghun16@naver.com>
2024-11-30 11:44:13 +0900

    update

---
+++ playground/boj_1264/boj_1264.py
@@ -0,0 +1,18 @@
+'''
+----------------------------------------------
+Sub : [BOJ] 모음의 개수
+Link: https://www.acmicpc.net/problem/1264
+Tag : Python, 
+Memo
+----------------------------------------------
+'''
+
+while True:
+    n = input()
+    if(n == '#'): 
+        break
+    else :
+        sum = 0
+        sum += n.count('a') + n.count('e') + n.count('i') + n.count('o') + n.count('u')
+
+    print(sum)
