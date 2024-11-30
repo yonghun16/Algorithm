c152bcf05fdd9ac1f40a076373fb41c9cfd01a7d
yonghun16 <yonghun16@naver.com>
2024-11-30 11:45:30 +0900

    update

--- playground/boj_1264/boj_1264.py
+++ playground/boj_1264/boj_1264.py
@@ -14,5 +14,6 @@ while True:
     else :
         sum = 0
         sum += n.count('a') + n.count('e') + n.count('i') + n.count('o') + n.count('u')
+        sum += n.count('A') + n.count('E') + n.count('I') + n.count('O') + n.count('U')
 
     print(sum)
