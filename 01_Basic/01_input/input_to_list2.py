58db346667e6a84cd598d9fb3be534209869fed3
yonghun16 <yonghun16@naver.com>
2024-11-30 21:01:39 +0900

    Update input_to_list2.py

--- 01_Basic/01_input/input_to_list2.py
+++ 01_Basic/01_input/input_to_list2.py
@@ -15,7 +15,8 @@ input style:
 
 # 입력
 n = int(input())
-inputDataList = [int(input()) for _ in range(n)]
+inputDataList = [int(input()) for _ in range(n)]      # 한 줄 마다 하나씩 받기
+#inputDataList = [int(w) for w in input().split()]    # 한 줄에 통으로 받기
 
 # 출력
 print(inputDataList)
