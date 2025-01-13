c8a31e99017b744d1079a427a5da9da87f781224
yonghun16 <yonghun16@naver.com>
2025-01-14 00:06:24 +0900

    Create boj_15874.py

---
+++ playground/BOJ/boj_15874_S/boj_15874.py
@@ -0,0 +1,32 @@
+'''
+----------------------------------------------
+Sub : [BOJ] Caesar Cipher
+Link: https://www.acmicpc.net/problem/15874
+Tag : Python, 
+Memo
+----------------------------------------------
+'''
+
+def caesar_cipher(k, s):
+    k = k % 26  # k가 26 이상일 경우를 대비해서, 26으로 나눈 나머지로 축소
+    result = []
+
+    for char in s:
+        if 'A' <= char <= 'Z':  # 대문자
+            new_char = chr(((ord(char) - ord('A') + k) % 26) + ord('A'))
+            result.append(new_char)
+        elif 'a' <= char <= 'z':  # 소문자
+            new_char = chr(((ord(char) - ord('a') + k) % 26) + ord('a'))
+            result.append(new_char)
+        else:
+            # 공백, 온점, 쉼표는 그대로 출력
+            result.append(char)
+
+    return ''.join(result)
+
+# 입력 받기
+k, n = map(int, input().split())
+s = input()
+
+# 암호화된 문자열 출력
+print(caesar_cipher(k, s))
