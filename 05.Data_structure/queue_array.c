0c8e714b58c7171063bb0cfb9fd1babf0827a72b
yonghun16 <yonghun16@naver.com>
2025-02-07 03:06:11 +0900

    update

---
+++ 05.Data_structure/queue_array.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+#define SIZE 5
+
+int queue[SIZE];
+int front = 0, rear = 0;
+
+int isFull() {
+    return rear == SIZE;
+}
+
+int isEmpty() {
+    return front == rear;
+}
+
+void enqueue(int value) {
+    if (isFull()) {
+        printf("Queue is full!\n");
+        return;
+    }
+    queue[rear++] = value;
+}
+
+int dequeue() {
+    if (isEmpty()) {
+        printf("Queue is empty!\n");
+        return -1;
+    }
+    int data = queue[0];
+    for (int i = 0; i < rear - 1; i++) {
+        queue[i] = queue[i + 1];
+    }
+    rear--;
+    return data;
+}
+
+void display() {
+    if (isEmpty()) {
+        printf("Queue is empty!\n");
+        return;
+    }
+    printf("\n");
+    printf("\n───────────────────────────────\n");
+    for (int i = 0; i < rear; i++) {
+        printf("%d ", queue[i]);
+    }
+    printf("\n───────────────────────────────\n\n");
+}
+
+int main() {
+    printf("───────────────────────────────\n");
+    printf("   Array 이용한 queue의 구현   \n");
+    printf("───────────────────────────────\n");
+    printf("\n");
+
+    int c, n;
+
+    while (1) {
+        printf("enqueue(1), dequeue(2), EXIT(0) : ");
+        scanf("%d", &c);
+        if (c == 1) {
+            printf("enqueue 할 숫자를 적으세요 : ");
+            scanf("%d", &n);
+            enqueue(n);
+            display();
+        } else if (c == 2) {
+            dequeue();
+            display();
+        } else if (c == 0) {
+            printf("프로그램을 종료합니다.\n");
+            break;
+        } else {
+            printf("메뉴의 숫자를 입력해 주세요.\n\n");
+            continue;
+        }
+    }
+    
+    return 0;
+}
--- 05.Data_structure/stack_array.c
+++ 05.Data_structure/stack_array.c
@@ -6,53 +6,61 @@
 int stack[SIZE];
 int top = -1;
 
+// 스택 요소 넣기
 void push(int data) {
-  if (top == SIZE - 1) {
-    printf("스텍 오버플로우가 발생했습니다.\n");
-    return;
-  }
-  stack[++top] = data;
+    if (top == SIZE - 1) {
+        printf("스텍 오버플로우가 발생했습니다.\n");
+        return;
+    }
+    stack[++top] = data;
 }
 
+// 요소 빼기
 int pop() {
-  if (top == -1) {
-    printf("스택 언더플로우가 발생했씁니다.\n");
-    return -INF;
-  }
-  return stack[top--];
+    if (top == -1) {
+        printf("스택 언더플로우가 발생했씁니다.\n");
+        return -INF;
+    }
+    return stack[top--];
 }
 
-void show() {
-  printf("\n   ↓ 스택의 최상단 \n");
-  printf("│     │\n");
-  for (int i = top; i >= 0; i--) {
-    printf("│%4d │\n", stack[i]);
-  }
-  printf("└─────┘\n");
+// 스택 보여주기
+void display() {
+    printf("\n   ↓ 스택의 최상단 \n");
+    printf("│     │\n");
+    for (int i = top; i >= 0; i--) {
+        printf("│%4d │\n", stack[i]);
+    }
+    printf("└─────┘\n");
 }
 
 int main() {
-  int c, n;
-  printf("───────────────────────────────\n");
-  printf("   배열을 이용한 스텍의 구현   \n");
-  printf("───────────────────────────────\n");
+    printf("───────────────────────────────\n");
+    printf("  Array을 이용한 Stack의 구현  \n");
+    printf("───────────────────────────────\n");
+    printf("\n");
 
-  while (1) {
-    printf("Push(1), Pop(2), EXIT(0) : ");
-    scanf("%d", &c);
-    if (c == 1) {
-      printf("Push 할 숫자를 적으세요 : ");
-      scanf("%d", &n);
-      push(n);
-      show();
-    } else if (c == 2) {
-      pop();
-      show();
-    } else if (c == 0) {
-      printf("프로그램을 종료합니다.\n");
-      break;
+    int c, n;
+
+    while (1) {
+        printf("Push(1), Pop(2), EXIT(0) : ");
+        scanf("%d", &c);
+        if (c == 1) {
+            printf("Push 할 숫자를 적으세요 : ");
+            scanf("%d", &n);
+            push(n);
+            display();
+        } else if (c == 2) {
+            pop();
+            display();
+        } else if (c == 0) {
+            printf("프로그램을 종료합니다.\n");
+            break;
+        } else {
+            printf("메뉴의 숫자를 입력해 주세요.\n\n");
+            continue;
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
