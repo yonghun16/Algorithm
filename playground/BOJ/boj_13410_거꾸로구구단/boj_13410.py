dc9ed64b7f23e4603dba008c2d030c569c77d87c
yonghun16 <yonghun16@naver.com>
2025-04-17 13:25:06 +0900

    Create boj_13410.py

---
+++ playground/BOJ/boj_13410_거꾸로구구단/boj_13410.py
@@ -0,0 +1,37 @@
+'''----------------------------------------------------
+Sub  : [BOJ] 거꾸로 구구단
+Link : https://www.acmicpc.net/problem/13410
+Level: 브론즈 2
+Tag  : Python, 구현
+Memo
+----------------------------------------------------'''
+
+import sys
+input = sys.stdin.readline
+
+
+N, K = map(int, input().strip().split())   # 입력 값
+maxNumber = 0
+answer = 0
+
+
+# 구구단 숫자를 거꾸로 구하기
+def reverseTimesTable(str):
+    resultNumber = []
+    for i in range(len(str)-1, -1, -1):
+        resultNumber.append(str[i])
+    return int(''.join(resultNumber))
+
+
+# 가장 큰 구구단 수 구하기
+for _ in range(2, K+1):
+    currentNumber = reverseTimesTable(str(N * _))
+
+    if (maxNumber < currentNumber):
+        maxNumber = currentNumber
+
+    answer = maxNumber
+
+
+# 결과 출력
+print(answer)
