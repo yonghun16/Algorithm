f0c99d161c4c37a3f06b28a13b66618e6e2b5584
yonghun16 <yonghun16@naver.com>
2025-04-28 14:39:27 +0900

    Update boj_2798.js

--- playground/BOJ/boj_2798/boj_2798.js
+++ playground/BOJ/boj_2798/boj_2798.js
@@ -4,14 +4,15 @@ Link : https://www.acmicpc.net/problem/2798
 Level: Bronze 2
 Tag  : JS, 브루트포스
 Memo
- - 
+ - 조합(콤비네이션)
+   n개의 카드 중에서 3개를 뽑는 조합으로 문제 풀이
 ----------------------------------------------*/
 
 const input = require('fs').readFileSync(0, 'utf-8').trim().split('\n')
 
 // 입력 받기
-const [N, M] = input[0].split(" ").map(Number);     //  3 <= N <= 100
-const cards = input[1].split(" ").map(Number);      // 10 <= M <= 300,000
+const [N, M] = input[0].split(" ").map(Number);     //  3 <= N <= 100,   10 <= M <= 300,000
+const cards = input[1].split(" ").map(Number);  
 
 // 출력 변수
 answer = 0;
@@ -23,18 +24,18 @@ function blackjack(N, M, cards) {
   let maxSum = 0;
 
   // 3중 for문으로 카드 3장을 선택
-  for (let i = 0; i < N-2; i++) {
-    for (let j = i + 1; j < N-1; j++) {
-      for (let k = j + 1; k < N; k++) {
-        const currentSum = cards[i] + cards[j] + cards[k];
-        if (currentSum <= M) {
-          maxSum = Math.max(maxSum, currentSum);
+  for (let i = 0; i < N-2; i++) {                           // 첫 번째 카드 뽑기
+    for (let j = i + 1; j < N-1; j++) {                     // 두 번째 카드 뽑기
+      for (let k = j + 1; k < N; k++) {                     // 세 번재 카드 뽑기
+        const currentSum = cards[i] + cards[j] + cards[k];  // 카드 3개 숫자 다 더하기
+        if (currentSum <= M) {                              // 카드 합이 M보다 같거나 작은 수에 한에서
+          maxSum = Math.max(maxSum, currentSum);            // 이전의 카드합과, 지금의 카드합 중 더 큰 것을 maxSum에 갱신
         }
       }
     }
   }
 
-  return maxSum;
+  return maxSum;                                            // 최종 maxSum을 반환
 }
 
 // 결과 출력
