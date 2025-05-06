ffb3d79006d3e7c9b65171930f2a63868e311a35
yonghun16 <yonghun16@naver.com>
2025-05-06 21:43:50 +0900

    boj_1269

--- playground/BOJ/boj_1269/boj_1269.py
+++ playground/BOJ/boj_1269/boj_1269.py
@@ -6,24 +6,37 @@ Tag  : Python,
 Memo
 ----------------------------------------------------'''
 
-import sys
-input = sys.stdin.readline
+# 입력 받기
 
-# input
-n, m = map(int, input().strip().split())
-A = list(map(int, input().strip().split()))
-B = list(map(int, input().strip().split()))
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
-# output
-answer = 0
+# 리스트 정렬
+A.sort()
+B.sort()
 
-# process
-C = A + B
+# 대칭 차집합 계산 (두 리스트를 투 포인터 방식으로 비교)
+i, j = 0, 0
 
-for c in C:
-    C.append(c)
-    if C.count(c) >= 2:
-        C.remove(c)
-        C.remove(c)
+symmetric_difference = 0
 
-print(len(C))
+while i < n and j < m:
+    if A[i] < B[j]:  # A의 원소가 B보다 작은 경우
+        symmetric_difference += 1
+        i += 1
+
+    elif A[i] > B[j]:  # B의 원소가 A보다 작은 경우
+        symmetric_difference += 1
+        j += 1
+
+    else:  # A[i] == B[j], 공통 원소이므로 제외
+           # symmetric_difference += 1  더하지 않는다.
+        i += 1
+        j += 1
+
+# 남아있는 원소 처리
+symmetric_difference += (n - i) + (m - j)
+
+# 결과 출력
+print(symmetric_difference)
