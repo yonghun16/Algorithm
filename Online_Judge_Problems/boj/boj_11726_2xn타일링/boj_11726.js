885edc79ae725d75a0af29cc095119506325af67
yonghun16 <yonghun16@naver.com>
2025-11-13 14:29:09 +0900

    li

--- Online_Judge_Problems/boj/boj_11726_2xn타일링/boj_11726.js
+++ Online_Judge_Problems/boj/boj_11726_2xn타일링/boj_11726.js
@@ -16,7 +16,7 @@ let input;
 if (TEST_MODE) {
   // 로컬 테스트 (input.txt 파일 읽기)
   const filePath = require("path").join(__dirname, "input.txt");
-  input = require("fs").readFileSync(filePath, "utf-7").trim().split("\n");
+  input = require("fs").readFileSync(filePath, "utf-8").trim().split("\n");
 } else {
   // 제출 환경 (표준 입력)
   input = require("fs").readFileSync(0, "utf-8").trim().split("\n");
